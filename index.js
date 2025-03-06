69713edd52c3d2a015859402e6a8f9d956b22940
AlexKi07 <kimanialexk07@gmail.com>
2025-03-06 17:04:15 +0300

    Assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+};
+
+mondayWork()
+mondayWork("work from home.")
+
+
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function wrapAdjective(wrapper) {
+    return function(adjective) {
+        return `You are ${wrapper}${adjective}${wrapper}!`;
+    };
+}
